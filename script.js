007ccd861c9fd2774304df3ffca32c40a80986fe
rnz.15 <suazolorenzo915.occ@gmail.com>
2022-09-25 17:21:24 +0800

    chnages in style

---
+++ script.js
@@ -0,0 +1,12 @@
+function mtblink() {
+    document.getElementById("title").innerHTML = "Mountain Bikes";
+  }
+  function rblink() {
+    document.getElementById("title").innerHTML = "Road Bike";
+  }
+  function comlink() {
+    document.getElementById("title").innerHTML = "Bike Components";
+  }
+  function comlink() {
+    document.getElementById("title").innerHTML = "About";
+  }
